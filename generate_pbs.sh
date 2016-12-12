5689b1194648a0f215fe4c0e386566f7088d38a7
Alex Pine <alpinesol@gmail.com>
2016-12-11 23:17:20 -0500

    fixed bug

--- generate_pbs.sh
+++ generate_pbs.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# This assumes your grumpy dir is in /scratch/${USER}/grumpy.
+
 if [ -z "$1" ]
   then
     echo "Usage: ./generate_pbs.sh [user_name] [script_name] [experiment_name] [data_dir] [extra flags]"
-    echo "e.g.: ./generate_pbs.sh akp258 train.py experiment_1 data --model=large --use_gru"
+    echo "e.g.: ./generate_pbs.sh akp258 experiment_1 data --model=large --use_gru"
     exit
 fi
 
 #Generate_pbs.sh
 user_name=$1 #eg akp258
-script_name=$2 #eg train.py
-experiment_name=$3 #eg experiment_1
-data_dir=$4 #eg data_dir
+experiment_name=$2 #eg experiment_1
+data_dir=$3 #eg data_dir
 
 echo "
 #!/bin/bash
@@ -29,7 +30,7 @@ module load pillow/intel/2.7.0
 module load tensorflow/python2.7/20161029
 module load scipy/intel/0.18.0
 
-cd ${HOME}/grumpy
-python ${script_name} --save=${data_dir}/${experiment_name}_$(date +'%m%d%H%M%S') ${@:4}
+cd /scratch/${USER}/grumpy
+python train.py --data=${data_dir} --save=save_dir/${experiment_name}_$(date +'%m%d%H%M%S') ${@:4}
 
 " > ${experiment_name}.pbs
